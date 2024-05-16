5cde0b6b0f22df79083fe4f1dc43822dd035cc5d
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-05-17 00:39:32 +0500

    Add AppLoader

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
 import HeroComponent from '@/components/HeroComponent.vue'
 import InternationForum from '@/components/InternationForum.vue'
 import ParticipantComponent from '@/components/ParticipantComponent.vue'
 import SpeakersComponent from '@/components/SpeakersComponent.vue'
 import AdvantagesComponent from '@/components/AdvantagesComponent.vue'
+import AppLoader from '@/components/AppLoader.vue'
+
+const isLoading = ref(true)
+
+onMounted(() => {
+  if (isLoading.value) document.body.style.overflow = 'hidden'
+})
+
+setTimeout(() => {
+  isLoading.value = false
+  document.body.style.overflow = 'auto'
+}, 1500)
 </script>
 
 <template>
+  <app-loader v-if="isLoading" />
   <div class="home">
     <the-header />
     <hero-component />
